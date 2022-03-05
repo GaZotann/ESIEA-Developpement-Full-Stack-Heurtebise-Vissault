8e70ca29184799dcf616c02f8826af1cd7985417
Gaëtan HEURTEBISE <heurtebise@et.esiea.fr>
2022-03-05 16:05:08 +0100

    Initialisation backend + gitignore

---
+++ Backend/HVapi/src/main/java/com/esiea/heurtebisevissault/api/HVapi/HVapiApplication.java
@@ -0,0 +1,13 @@
+package com.esiea.heurtebisevissault.api.HVapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HVapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HVapiApplication.class, args);
+	}
+
+}
---
+++ Backend/HVapi/src/test/java/com/esiea/heurtebisevissault/api/HVapi/HVapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiea.heurtebisevissault.api.HVapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HVapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
